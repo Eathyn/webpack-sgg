494bae5f96bf3dfddeb93b3615411bc43bea80ad
Eathyn <eathynzakas@gmail.com>
2022-06-16 10:17:34 +0800

    10: handle sass or scss file
    sass-loader: sass/scss 编译为 css
    sass: sass-loader 依赖 sass 进行编译

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,12 @@ module.exports = {
     filename: 'main.js',
   },
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [],
   mode: 'development',
