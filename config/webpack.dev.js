5c97abe7b0a51ab7a080b324d43fb8f07ff346fe
Eathyn <eathynzakas@gmail.com>
2022-06-21 09:29:05 +0800

    20: development and production modes

---
+++ config/webpack.dev.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const ESLintWebpackPlugin = require('eslint-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: undefined,
+    filename: 'static/js/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 30 * 1024, // 小于 30kb 的图片会被 base64 处理
+          },
+        },
+        generator: {
+          filename: 'static/images/[hash:8][ext][query]',
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2|mp4|mp3|avi)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/media/[hash:8][ext][query]',
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ESLintWebpackPlugin({
+      context: path.resolve(__dirname, '../src'),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+  },
+  mode: 'development',
+}
