1732358572b98f9a09246a3a4d101933762a8033
Eathyn <eathynzakas@gmail.com>
2022-06-25 06:39:06 +0800

    23.2: oneOf

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -12,48 +12,52 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
+				oneOf: [
 					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: [['postcss-preset-env']],
+						test: /\.css$/,
+						use: [
+							MiniCssExtractPlugin.loader,
+							'css-loader',
+							{
+								loader: 'postcss-loader',
+								options: {
+									postcssOptions: {
+										plugins: [['postcss-preset-env']],
+									},
+								},
+							},
+						],
+					},
+					{
+						test: /\.(png|jpe?g|gif|webp)$/,
+						type: 'asset',
+						parser: {
+							dataUrlCondition: {
+								maxSize: 30 * 1024, // 小于 30kb 的图片会被 base64 处理
+							},
+						},
+						generator: {
+							filename: 'static/images/[hash:8][ext][query]',
+						},
+					},
+					{
+						test: /\.(ttf|woff|woff2|mp4|mp3|avi)$/,
+						type: 'asset/resource',
+						generator: {
+							filename: 'static/media/[hash:8][ext][query]',
+						},
+					},
+					{
+						test: /\.m?js$/,
+						exclude: /node_modules/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env'],
 							},
 						},
 					},
 				],
-			},
-			{
-				test: /\.(png|jpe?g|gif|webp)$/,
-				type: 'asset',
-				parser: {
-					dataUrlCondition: {
-						maxSize: 30 * 1024, // 小于 30kb 的图片会被 base64 处理
-					},
-				},
-				generator: {
-					filename: 'static/images/[hash:8][ext][query]',
-				},
-			},
-			{
-				test: /\.(ttf|woff|woff2|mp4|mp3|avi)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'static/media/[hash:8][ext][query]',
-				},
-			},
-			{
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
 			},
 		],
 	},
