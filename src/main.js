7e3ebd575e122a2b40a6129d09c1106503cdff44
Eathyn <eathynzakas@gmail.com>
2022-06-30 10:37:58 +0800

    25.2: 动态导入+命名

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { sum } from './js/utils'
-
 import './css/index.css'
 import './css/test.css'
 import './css/iconfont.css'
 
-console.log(sum(1, 2, 3))
+const btn1 = document.querySelector('.test-dynamically-import')
+btn1.addEventListener('click', () => {
+  import(/* webpackChunkName: 'utils' */ './js/utils').then(({ sum }) => {
+    console.log(sum(1, 2, 3, 4, 5))
+  })
+})
